c7c524385dc9f8dd81b041d3639408a7244d2a06
ben <beanie@benle.de>
2017-10-04 09:12:09 -0300

    inital version

---
+++ mintos.lua
@@ -0,0 +1,64 @@
+WebBanking{
+  version = 1.0,
+  url = "https://www.mintos.com/",
+  description = "Mintos",
+  services= { "Mintos Passive Account" },
+}
+
+local currency = "EUR" -- fixme: Don't hardcode
+local currencyName = "EUR" -- fixme: Don't hardcode
+local connection
+local apiKey
+
+function SupportsBank (protocol, bankCode)
+  return protocol == ProtocolWebBanking and bankCode == "Mintos Passive Account"
+end
+
+function InitializeSession (protocol, bankCode, username, username2, password, username3)
+  connection = Connection()
+  local html = HTML(connection:get(url))
+  local csrfToken = html:xpath("//*[@id='login-form']/input[@name='_csrf_token']"):val()
+
+  content, charset, mimeType = connection:request("POST",
+  "https://www.mintos.com/en/login/check",
+  "_username=" .. username .. "&_password=" .. password .. "&_csrf_token=" .. csrfToken,
+  "application/x-www-form-urlencoded; charset=UTF-8")
+
+  if string.match(connection:getBaseURL(), 'login') then
+      return LoginFailed
+  end
+end
+
+function ListAccounts (knownAccounts)
+  local account = {
+    name = "Mintos",
+    accountNumber = "Mintos",
+    currency = currency,
+    portfolio = true,
+    type = "AccountTypePortfolio"
+  }
+
+  return {account}
+end
+
+function RefreshAccount (account, since)
+  local s = {}
+  content = HTML(connection:get("https://www.mintos.com/en/overview/"))
+  total = content:xpath('//*[@id="mintos-boxes"]/li[1]/div/div[1]/div'):text()
+  total = string.gsub(total, "€", "")
+  total = string.gsub(total, " ", "")
+
+  local security = {
+    name = "Account Summary",
+    price = tonumber(total),
+    quantity = 1,
+    curreny = nil,
+  }
+
+  table.insert(s, security)
+
+  return {securities = s}
+end
+
+function EndSession ()
+end
